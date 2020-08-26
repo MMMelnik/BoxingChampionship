40ecfa91e367828a5477bbb978aa3952c0504125
Mykola Melnyk <kolanio757@gmail.com>
2020-08-27 02:45:54 +0300

    add fighters' rank on the home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Threading;
+using System.Web.Mvc;
+using BoxingChampionship.Models;
+using BoxingChampionship.ViewModels;
+using WebGrease.Css.Ast.Selectors;
 
 namespace BoxingChampionship.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly ChampionshipContext Db = new ChampionshipContext();
+        private static List<ChampionshipRankViewModel> _championshipRankViewModels;
+
         public ActionResult Index()
         {
-            return View();
+            return View(_championshipRankViewModels);
+        }
+
+        public HomeController()
+        {
+            GenerateRank();
+        }
+
+        private static void GenerateRank()
+        {
+            var battles = Db.Battles.ToList();
+            _championshipRankViewModels = battles
+                .GroupBy(b => b.Winner)
+                .Select(r => new ChampionshipRankViewModel
+                {
+                    AmountOfBattles = r.Count()+(battles.Count(bt => bt.Loser == r.First().Winner)),
+                    Name = r.First().Winner,
+                    CurrentRanking = r.Sum(b => b.RefereePoints),
+                }).OrderByDescending(cr => cr.CurrentRanking).ToList();
         }
     }
 }
---
+++ ViewModels/ChampionshipRankViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BoxingChampionship.ViewModels
+{
+    public class ChampionshipRankViewModel
+    { 
+        public int CurrentRanking { get; set; }
+        public string Name { get; set; }
+        public int AmountOfBattles { get; set; }
+    }
+}
